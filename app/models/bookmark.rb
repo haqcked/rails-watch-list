e703775483dbef8c8e69128f01a8ba859c961edc
abdulhaq abdullah <haqabdullahov@gmail.com>
2023-02-11 19:13:40 +0600

    added form in the list show

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,7 +2,7 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
-  validates :comment, length: { minimum: 6 }
+  validates :comment, length: { minimum: 4 }
   validates :movie, presence: true
   validates :list, presence: true
   validates :movie_id, uniqueness: { scope: :list_id }
